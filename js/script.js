686adc3078dc9bf8b44715195651827c9124d6a3
MrCherry89 <abgarnati@yandex.ru>
2024-11-12 03:43:48 +0400

    ok

--- js/script.js
+++ js/script.js
@@ -9,6 +9,60 @@ $(document).ready(function () {
     nextArrow: $(".unique-features-slider-wrap .slider-navigation .slick-next"),
   });
 
+  $(".advantages-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    variableWidth: true,
+    centerMode: true,
+    prevArrow: $(".advantages .slider-navigation .slick-prev"),
+    nextArrow: $(".advantages .slider-navigation .slick-next"),
+  });
+
+  $(".videos-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    variableWidth: true,
+    centerMode: true,
+    prevArrow: $(".videos .slider-navigation .slick-prev"),
+    nextArrow: $(".videos .slider-navigation .slick-next"),
+  });
+
+  $(".phone-number-input").inputmask({
+    mask: "+7 (999)-999-999-9",
+  });
+
+  $("#application-form").validate({
+    errorClass: "error", // Класс для ошибок
+    errorPlacement: function (error, element) {
+      // Оставляем пустым, чтобы не показывать текст ошибки
+    },
+    highlight: function (element) {
+      $(element).addClass("error");
+    },
+    unhighlight: function (element) {
+      $(element).removeClass("error");
+    },
+    submitHandler: function (form) {
+      // Действия при успешной валидации
+      $("#application-form").hide(); // Скрываем первую форму
+      $("#second-form").show(); // Показываем вторую форму
+    },
+  });
+
+  // Кнопка отправки для первой формы
+  $("#first-form-submit").on("click", function (e) {
+    e.preventDefault(); // Предотвращаем стандартное поведение
+
+    // Проверка первой формы
+    if ($("#application-form").valid()) {
+      $("#application-form").submit(); // Вызываем обработчик при успешной валидации
+    }
+  });
+
   $(".tab-link").click(function () {
     const tabId = $(this).data("tab");
 
@@ -23,5 +77,16 @@ $(document).ready(function () {
     setTimeout(function () {
       $("#" + tabId).addClass("active");
     }, 0);
+  });
+
+  $(".select-wrap select").select2({
+    minimumResultsForSearch: 6,
+  });
+
+  $(".drop-menu").click(function (e) {
+    e.stopPropagation();
+    $(this).toggleClass("is-active");
+    $(".menu-wrap").toggleClass("open");
+    $("body, html").toggleClass("overflow");
   });
 });
